030c825cdbefdcc8a586c718ed07bddcd78c30cc
VelimirZG <vzagar@hotmail.com>
2024-03-11 12:38:32 +0100

    new wallet3

--- frontend-poc/src/assets/near/utils.js
+++ frontend-poc/src/assets/near/utils.js
@@ -2,7 +2,7 @@ import { connect, Contract, keyStores, WalletConnection } from 'near-api-js'
 import getConfig from './config'
 
 
-const nearConfig = getConfig('development');
+const nearConfig = getConfig('production');
 console.log('CONFIG: ', nearConfig);
 
 // Initialize contract & set global variables
